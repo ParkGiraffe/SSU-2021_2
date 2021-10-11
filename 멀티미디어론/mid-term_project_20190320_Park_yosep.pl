6773644d215b413886fc06b43ccd8c4af6f1ffd1
ParkGiraffe <op5321@soongsil.ac.kr>
2021-10-11 16:50:42 +0900

    feat: 멀티미디어론 중간과제 1차 완료

--- 멀티미디어론/mid-term_project_20190320_Park_yosep.pl
+++ 멀티미디어론/mid-term_project_20190320_Park_yosep.pl
@@ -7,7 +7,22 @@ use_env(indoor) :- ( use_loc(home); use_loc(company_hold); use_loc(car); ).
 use_env(outdoor) :- ( use_loc(school); use_loc(cafe); use_loc(company_commute); ).
 
 brand(apple) :- (   prefOS(iPad_OS);   ).
-brand(google) :- (  prefOS(android);    ).
+brand(samsung) :- (  prefOS(android);    ).
 
+tablet(iPad_pro_12.9_5gen_apple_pencil) :- ( use(productivity), use_env(indoor), brand(apple) ).
+tablet(Galaxy_tab_S7_plus) :- ( use(productivity), use_env(indoor), brand(samsung) ). 
+tablet(iPad_pro_11_3gen_apple_pencil) :- ( use(productivity), use_env(outdoor), brand(apple) ). 
+tablet(Galaxy_tab_S7) :- ( use(productivity), use_env(outdoor), brand(samsung) ). 
+
+tablet(iPad_air_4gen) :- ( use(simwork_comsum), use_env(indoor), brand(apple) ).
+tablet(Galaxy_tab_S7_FE) :- ( use(simwork_comsum), use_env(indoor), brand(samsung) ).
+tablet(iPad_mini_6gen) :- ( use(simwork_comsum), use_env(outdoor), brand(apple) ).
+tablet(Galaxy_tab_S5e) :- ( use(simwork_comsum), use_env(outdoor), brand(samsung) ).
+
+tablet(iPad_pro_11_3gen) :- ( use(game), use_env(indoor) ).
+tablet(iPad_mini_6gen) :- ( use(game), use_env(outdoor) ).
+
+tablet(iPad_9gen) :- ( use(etc), brand(apple) ).
+tablet(Galaxy_tab_A7_Lite) :- ( use(etc), brand(samsung) ).
 
 
