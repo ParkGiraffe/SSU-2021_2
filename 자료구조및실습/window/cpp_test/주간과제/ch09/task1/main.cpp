b440e336e2f1ad0bc7d8b8356acbb62a33c2c069
ParkGiraffe <op5321@soongsil.ac.kr>
2021-11-22 21:59:07 +0900

    feat: 자료구조및실습 12주차 과제 - 원형 연결 리스트를 사용한 큐, 그리고 이거를 활용하여 요세푸스의 문제 해결 프로그램 작성

---
+++ 자료구조및실습/window/cpp_test/주간과제/ch09/task1/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "QueueByCircularLinkedList.h"
+
+using namespace std;
+
+int main() {
+    const int N = 41;
+    const int M = 3;
+
+    QueueByCircularLinkedList Q;
+
+    // Josephus의 문제 로직
+    for (int i = 0; i < 41; i++) {
+        Q.enQueue(i+1);
+    }
+
+    while(Q.GetSize() >= M) {
+        Q.enQueue(Q.GetFront());
+        Q.deQueue();
+        Q.enQueue(Q.GetFront());
+        Q.deQueue();
+        Q.deQueue();
+    }
+
+
+    cout << "마지막 생존자가 앉은 자리의 번호는 ";
+    while (!Q.IsEmpty()) {
+        cout << Q.deQueue();
+
+        if (!Q.IsEmpty())
+            cout << ", ";
+    }
+
+    cout << " 입니다.";
+
+    return 0;
+}
