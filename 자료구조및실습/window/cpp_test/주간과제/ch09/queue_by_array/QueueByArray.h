02c4f92bd69ca1faf9f5e3d551d7aa620344134c
ParkGiraffe <op5321@soongsil.ac.kr>
2021-11-22 20:20:43 +0900

    feat: 자료구조및실습 12주차 과제 - 배열을 사용한 큐, 그리고 이거를 활용하여 회문 검사 프로그램 만들기

---
+++ 자료구조및실습/window/cpp_test/주간과제/ch09/queue_by_array/QueueByArray.h
@@ -0,0 +1,23 @@
+#pragma once
+const int MAX = 100;
+
+
+class QueueByArray
+{
+private :
+    int front, rear;
+    int count;
+    char Queue[MAX];
+
+
+public :
+    QueueByArray(void);
+    ~QueueByArray(void);
+
+    void enQueue(char Item);
+    char deQueue();
+    bool IsEmpty();
+    bool IsFull();
+    char GetFront();
+    int GetSize();
+};
